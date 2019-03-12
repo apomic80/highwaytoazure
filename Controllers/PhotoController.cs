56982d722532850ddfec37cc529ac35116920eb3
apomic80 <michele.aponte@blexin.com>
2019-03-12 02:27:02 +0100

    content moderation

--- Controllers/PhotoController.cs
+++ Controllers/PhotoController.cs
@@ -8,6 +8,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using demo.Shared;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Azure.CognitiveServices.ContentModerator;
+using Microsoft.Azure.CognitiveServices.ContentModerator.Models;
 
 namespace demo.Controllers
 {
@@ -41,6 +45,12 @@ namespace demo.Controllers
         {
             try
             {
+                // var result = await checkImage(file);
+
+                var result = checkImageFaces(file);
+
+                var result2 = checkText(model.Description);
+
                 var uploads = "uploads";
                 if(!Directory.Exists(uploads))
                 {
@@ -83,6 +93,82 @@ namespace demo.Controllers
             {
                 return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
             }
+        }
+
+        private async Task<string> checkImage(IFormFile file) 
+        {
+            var uri = "https://westeurope.api.cognitive.microsoft.com/contentmoderator/moderate/v1.0/ProcessImage/Evaluate";
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add(
+                "Ocp-Apicription-Keym-Subs",
+                "292d6f7673d54abeb7ae1c4bdf1ef410"
+            );
+
+            HttpResponseMessage response = null;
+            using(var byteArray = new MemoryStream())
+            {
+                file.CopyTo(byteArray);
+                using(var content = new ByteArrayContent(byteArray.ToArray())) 
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+                    response = await client.PostAsync(uri, content);
+                    return await response.Content.ReadAsStringAsync(); 
+                }
+            }
+        }
+
+        private Evaluate checkImage2(IFormFile file)
+        {
+            var client = new ContentModeratorClient(
+                new ApiKeyServiceClientCredentials("292d6f7673d54abeb7ae1c4bdf1ef410"));
+            client.Endpoint = "https://westeurope.api.cognitive.microsoft.com";
+        
+            Evaluate response = client.ImageModeration.EvaluateFileInput(file.OpenReadStream());
+            return response;
+        }
+
+        private OCR checkImageOCR(IFormFile file)
+        {
+            var client = new ContentModeratorClient(
+                new ApiKeyServiceClientCredentials("292d6f7673d54abeb7ae1c4bdf1ef410"));
+            client.Endpoint = "https://westeurope.api.cognitive.microsoft.com";
+        
+            var response = client.ImageModeration
+                .OCRFileInput("ita", file.OpenReadStream());
+            return response;
+        }
+
+        private FoundFaces checkImageFaces(IFormFile file)
+        {
+            var client = new ContentModeratorClient(
+                new ApiKeyServiceClientCredentials("292d6f7673d54abeb7ae1c4bdf1ef410"));
+            client.Endpoint = "https://westeurope.api.cognitive.microsoft.com";
+        
+            var response = client.ImageModeration
+                .FindFacesFileInput(file.OpenReadStream());
+            return response;
+        }
+
+         private Screen checkText(string text)
+        {
+            var client = new ContentModeratorClient(
+                new ApiKeyServiceClientCredentials("292d6f7673d54abeb7ae1c4bdf1ef410"));
+            client.Endpoint = "https://westeurope.api.cognitive.microsoft.com";
+        
+            var response = client.TextModeration
+                .ScreenText("text/plain", this.generateStreamFromString(text), "ita", true, true, null, true);
+            return response;
+        }
+
+        private Stream generateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
         }
 
     }
